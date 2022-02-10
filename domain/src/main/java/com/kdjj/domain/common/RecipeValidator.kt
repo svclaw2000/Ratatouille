275124132cf356f3b9b5540901958efa048aa779
Keich.Park <svclaw2000@gmail.com>
2022-02-10 22:13:52 +0900

    feat: #5 검증 로직 Domain Layer로 이동

---
+++ domain/src/main/java/com/kdjj/domain/common/RecipeValidator.kt
@@ -0,0 +1,14 @@
+package com.kdjj.domain.common
+
+import javax.inject.Inject
+
+class RecipeValidator @Inject constructor() {
+
+    fun validateTitle(title: String): Boolean {
+        return title.isNotBlank()
+    }
+
+    fun validateStuff(stuff: String): Boolean {
+        return stuff.isNotBlank()
+    }
+}
