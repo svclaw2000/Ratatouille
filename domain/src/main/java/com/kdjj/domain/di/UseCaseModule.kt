461e8c0aaaeea670b968cade678a9d1f65292536
Keich.Park <svclaw2000@gmail.com>
2022-02-14 17:37:13 +0900

    feat: #5 레시피 단계 실시간 검증 UseCase 추가

--- domain/src/main/java/com/kdjj/domain/di/UseCaseModule.kt
+++ domain/src/main/java/com/kdjj/domain/di/UseCaseModule.kt
@@ -2,6 +2,7 @@ package com.kdjj.domain.di
 
 import com.kdjj.domain.model.request.*
 import com.kdjj.domain.model.response.ValidateRecipeFlowResponse
+import com.kdjj.domain.model.response.ValidateRecipeStepFlowResponse
 import com.kdjj.domain.usecase.*
 import dagger.Binds
 import dagger.Module
@@ -13,4 +14,9 @@ abstract class UseCaseModule {
     internal abstract fun bindValidateRecipeFlowUseCase(
         validateRecipeFlowUseCase: ValidateRecipeFlowUseCase
     ) : UseCase<ValidateRecipeFlowRequest, ValidateRecipeFlowResponse>
+
+    @Binds
+    internal abstract fun bindValidateRecipeStepFlowUseCase(
+        validateRecipeStepFlowUseCase: ValidateRecipeStepFlowUseCase
+    ) : UseCase<ValidateRecipeStepFlowRequest, ValidateRecipeStepFlowResponse>
 }
