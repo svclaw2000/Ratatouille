396a6694190c7484f246bf5ba940744f872ea864
Keich.Park <svclaw2000@gmail.com>
2022-02-25 02:18:16 +0900

    refactor: #9 사용하지 않는 UploadException 클래스 제거

--- domain/src/main/java/com/kdjj/domain/model/exception/UploadException.kt
+++
@@ -1,9 +0,0 @@
-package com.kdjj.domain.model.exception
-
-import com.kdjj.domain.model.Recipe
-import java.lang.Exception
-
-class UploadException(
-    msg: String? = null,
-    val recipe: Recipe
-) : Exception(msg ?: "upload fail")
